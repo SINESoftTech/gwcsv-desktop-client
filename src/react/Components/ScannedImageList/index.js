d4285f9e4780e041aa631d0087dfaf142c0ad260
Sean Lin <seanlin816@gmail.com>
2021-07-26 10:55:18 +0800

    image list first step

--- src/react/Components/ScannedImageList/index.js
+++ src/react/Components/ScannedImageList/index.js
@@ -6,6 +6,9 @@ import scannedImageListStyles from "./scannedImageListStyles";
 import ColumnDefinitions from "./columnDefinitions";
 import {DataGrid} from "@material-ui/data-grid";
 import {sequence} from "ramda";
+import {IconButton, ImageList, ImageListItem, ImageListItemBar} from "@material-ui/core";
+import {Info as InfoIcon} from '@material-ui/icons';
+
 const electron = isElectron() ? window.electron : null;
 const remote = isElectron() ? window.remote : null;
 const ipcRenderer = isElectron() ? electron.ipcRenderer : null
@@ -18,6 +21,13 @@ const sendToIdentify = async (fileObj) => {
       id: '8c869dbd-1bb5-4fe2-ac60-9b4215edffb8'
     }
   }
+}
+
+const getRowData = (fileNames) => {
+  return fileNames.map((item, index) => {
+    console.log('in render dataRow item', item)
+    return {id: index + 1, fileName: item.filename, imageUrl: ''}
+  })
 }
 
 const ScannedImageList = (props) => {
@@ -44,20 +54,31 @@ const ScannedImageList = (props) => {
     }
   };
 
-  const dataRows = (props.data) ? props.data.map((item, index) => {
-    console.log('in render dataRow item', item)
-    return {id: index+1, fileName: item.filename}
-  }) : []
+  const dataRows = (props.data) ? getRowData(props.data) : []
   console.log('dataRows', dataRows)
   const columns = ColumnDefinitions
 
   return (
-      <div style={{height: 650, width: '100%'}}>
-        <Button variant="contained" onClick={props.onScanClick}>掃描文件</Button>
-        <Button variant="contained" onClick={props.onSendToIdentifyClick}>送出辨識</Button>
-        <DataGrid rows={dataRows}
-                  columns={columns}
-        />
+    <div style={{height: 650, width: '100%'}}>
+      <Button variant="contained" onClick={props.onScanClick}>掃描文件</Button>
+      <Button variant="contained" onClick={props.onSendToIdentifyClick}>送出辨識</Button>
+      <div className={classes.root}>
+        <ImageList rowHeight={180} className={classes.imageList}>
+          {dataRows.map((item) => (
+            <ImageListItem key={item.id}>
+              <img src={item.imageUrl} alt={item.fileName} />
+              <ImageListItemBar
+                title={item.fileName}
+                actionIcon={
+                  <IconButton aria-label={`info about ${item.fileName}`} className={classes.icon}>
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </div>
     </div>
   );
 };
--- src/react/Components/ScannedImageList/scannedImageListStyles.js
+++ src/react/Components/ScannedImageList/scannedImageListStyles.js
@@ -4,6 +4,20 @@ const scannedImageListStyles = makeStyles((theme) => ({
     zIndex: theme.zIndex.drawer + 1,
     color: '#fff',
   },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  imageList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
 }));
 
 export default scannedImageListStyles
