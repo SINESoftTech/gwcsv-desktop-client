c27440d65d2b45688a2e06e62d814dc36f3c84e3
Tony <tony.wu.gateweb@gmail.com>
2021-08-17 14:03:42 +0800

    fixed sightour getToken api

--- src/react/Actions/sightourActions.js
+++ src/react/Actions/sightourActions.js
@@ -1,9 +1,24 @@
-import {signtTourAxios} from "./axios";
-import axios from "axios";
+import { signtTourAxios } from './axios'
+import axios from 'axios'
 
 const R = require('ramda')
 
+const getToken = (id, psw) => {
+  try {
+    const apiPath = '/requestToken.php'
+    const req = {
+      'id': id,
+      'psw': psw
+    }
+    const result = signtTourAxios.post(apiPath, JSON.stringify(req))
+    console.log(result)
+  } catch (error) {
+    //TODO error handle
+  }
+}
+
 export async function sendToIdentify(dispatch, fileObjectList) {
+  console.log(fileObjectList)
   try {
 
   } catch (error) {
@@ -11,25 +26,25 @@ export async function sendToIdentify(dispatch, fileObjectList) {
 }
 
 export async function getIdentifyResult(dispatch, payload) {
-  dispatch({type: 'LOGOUT'});
+  dispatch({ type: 'LOGOUT' })
 }
 
 export async function sendConfirmedResult(dispatch, payload) {
-  dispatch({type: 'LOGOUT'});
+  dispatch({ type: 'LOGOUT' })
 }
 
 export const handleSendImageToIdentify = (data) => {
   //FIXME
   const blob = data[0].imageUrl
   const fileName = data[0].fileName
-  const file = new File([blob], fileName);
+  const file = new File([blob], fileName)
   //FIXME add value
-  const bodyFormData = new FormData();
-  bodyFormData.append('file', file);
-  bodyFormData.append('type', "A5001");
-  bodyFormData.append('agent', "00000000");
-  bodyFormData.append('company', "00000000");
-  bodyFormData.append('token', "2olhx7gwv10z");
+  const bodyFormData = new FormData()
+  bodyFormData.append('file', file)
+  bodyFormData.append('type', 'A5001')
+  bodyFormData.append('agent', '00000000')
+  bodyFormData.append('company', '00000000')
+  bodyFormData.append('token', '2olhx7gwv10z')
   //todo upload
   console.log(bodyFormData)
   uploadFileToSightour(bodyFormData)
