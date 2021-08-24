b9dcc099e11c037710d48f9ba3c77dfcf5c5239b
Tony <tony.wu.gateweb@gmail.com>
2021-08-24 09:55:11 +0800

    add todo

--- src/react/Components/EvidenceListTable/ColumnDefinitions.js
+++ src/react/Components/EvidenceListTable/ColumnDefinitions.js
@@ -13,11 +13,12 @@ const ColumnDefinitions: GridColDef[] = [
   { field: 'buyerTaxId', headerName: '買方統編', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'sellerTaxId', headerName: '賣方統編', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'taxType', headerName: '課稅別', width: 150, cellClassName: getCellClassName, editable: true },
-  { field: 'totalAmount', headerName: '總額', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'taxableSalesValue', headerName: '應稅銷售額', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'dutyFreeSalesValue', headerName: '免稅銷售額', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'zeroTaxSalesValue', headerName: '零税銷售額', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'businessTaxValue', headerName: '税額', width: 150, cellClassName: getCellClassName, editable: true },
+  { field: 'otherFee', headerName: '其他金額', width: 150, cellClassName: getCellClassName, editable: true },
+  { field: 'totalAmount', headerName: '總額', width: 150, cellClassName: getCellClassName, editable: true },
   { field: 'rowStatus', hide: true },
   { field: 'cellHighlight', hide: true }
 ]
