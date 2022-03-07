f3df2ed581f7db69c2d82a856c9e9d3c271f9d97
Tony <se11@gateweb.com>
2022-03-07 18:09:46 +0800

    fixed

--- src/react/Actions/axios.js
+++ src/react/Actions/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const SIGHT_TOUR_ROOT_URL = 'http://aiocr.sightour.com/gateweb/api'
-const GW_ROOT_URL = 'http://104.155.204.250:8888'
+const GW_ROOT_URL = 'http://gwvatdemo.tweinv.com:8000'
 const gwAxios = axios.create({
   baseURL: GW_ROOT_URL,
   headers: { 'Content-Type': 'application/json' }
