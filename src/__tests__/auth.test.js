9a443037677cc01f74866eb440124372a73b364b
Sean Lin <seanlin@gateweb.com.tw>
2022-11-17 08:46:29 +0800

    add token expire detect

--- src/__tests__/auth.test.js
+++ src/__tests__/auth.test.js
@@ -1,18 +1,11 @@
-import jwt_decode from 'jwt-decode'
 import moment from 'moment'
+import {isTokenExpired, getTokenExpDate, getTokenInfo} from "../react/Util/authUtils";
 
-const isTokenExpired = (token) => {
-    const result = jwt_decode(token)
-    const expDate = moment(new Date(result.exp * 1000)).utcOffset(8)
-    const now = moment().utcOffset(8)
-    const dateDiff = now.diff(expDate)
-    return dateDiff >=0
-}
 describe('auth test', ()=>{
     it('should decipher jwt token',  () => {
         const token = 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjY3YzdmOTI3LTk1YjktNDMxZC1iMjU4LTA1YjQzNjMwM2IzMCIsImV4cCI6MTY2ODYxNjQ5MywiaXNzIjoiR2F0ZXdlYiJ9.83TlpqO-NBxw3l460s5L41H_pWBPvUvbiZor_MQJBhk'
-        const isExpired = isTokenExpired(token)
-        expect(isExpired).toBe(false)
-
+        const token2 = 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjY3YzdmOTI3LTk1YjktNDMxZC1iMjU4LTA1YjQzNjMwM2IzMCIsImV4cCI6MTY2ODY1MDY4MCwiaXNzIjoiR2F0ZXdlYiJ9.jtmBCwSlQALT_mO78HwyrmyMEKoT6kptR1LP4re911I'
+        const parsedToken = getTokenInfo(token2)
+        console.log('parsedToken', parsedToken)
     });
 })
--- src/react/Components/ProtectedRoute.js
+++ src/react/Components/ProtectedRoute.js
@@ -1,13 +1,12 @@
 import {useAppDispatch, useAppState} from "../Context";
 import {Navigate} from "react-router-dom";
+import {getTokenExpDate, getTokenInfo, isTokenExpired} from "../Util/authUtils";
 
 const ProtectedRoute = ({children}) => {
-  const dispatch = useAppDispatch();
   const {auth} = useAppState();
-  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-  console.log('currentUser', currentUser);
-  console.log('auth.user.token', auth.user.token)
-  if (!auth.user.token) {
+  const tokenInfo = getTokenInfo(auth.user.token);
+
+  if (tokenInfo.isExpired) {
     // user is not authenticated
     return <Navigate to="/login"/>;
   }
