a6c53b808811aefa7bfb45d0d22940823e10c171
Tony <se11@gateweb.com>
2022-11-23 14:33:12 +0800

    fixed

--- src/react/Components/ColumnDefinitions.js
+++ src/react/Components/ColumnDefinitions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FormControl, IconButton, MenuItem, Select,} from '@mui/material'
+import {IconButton,} from '@mui/material'
 import {GW_EVIDENCE_TYPE} from '../Mapper/gw_mapper';
 import {Delete, ImageSearch} from "@mui/icons-material";
 
@@ -71,21 +71,21 @@ export const IdentifiedEvidenceColumnDefinitions = [
         field: 'image',
         headerName: '圖檔',
         width: 70,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: false,
         renderCell: (cellValues) => <ImageSearch color='primary'>打開</ImageSearch>
     },
     {
-        field: 'sn', headerName: '序號', width: 110, cellClassName: getCellClassName, editable: false,
+        field: 'sn', headerName: '序號', width: 110, cellClassName: renderCellClassName, editable: false,
     },
     {
-        field: 'reportingPeriod', headerName: '申報期別', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'reportingPeriod', headerName: '申報期別', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
         field: 'evidenceType',
         headerName: '憑證類型',
         width: 200,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: true,
         type: 'singleSelect',
         valueOptions: renderEvidenceType()
@@ -95,65 +95,65 @@ export const IdentifiedEvidenceColumnDefinitions = [
         field: 'taxType',
         headerName: '課稅別',
         width: 150,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: true,
     },
     //todo
     // {
-    //     field: 'other', headerName: '其他代收代付', width: 110, cellClassName: getCellClassName, editable: true,
+    //     field: 'other', headerName: '其他代收代付', width: 110, cellClassName: renderCellClassName, editable: true,
     // },
     {
-        field: 'evidenceDate', headerName: '憑證日期', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'evidenceDate', headerName: '憑證日期', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
-        field: 'evidenceNumber', headerName: '憑證號碼', width: 200, cellClassName: getCellClassName, editable: true,
+        field: 'evidenceNumber', headerName: '憑證號碼', width: 200, cellClassName: renderCellClassName, editable: true,
     },
     {
-        field: 'buyerTaxId', headerName: '買方統編', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'buyerTaxId', headerName: '買方統編', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
-        field: 'sellerTaxId', headerName: '賣方統編', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'sellerTaxId', headerName: '賣方統編', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
         field: 'taxableSalesValue',
         headerName: '應稅銷售額',
         width: 150,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: true,
     },
     {
         field: 'dutyFreeSalesValue',
         headerName: '免稅銷售額',
         width: 150,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: true,
     },
     {
         field: 'zeroTaxSalesValue',
         headerName: '零税銷售額',
         width: 150,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: true,
     },
 
     {
-        field: 'businessTaxValue', headerName: '税額', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'businessTaxValue', headerName: '税額', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
         field: 'deductionType',
         headerName: '扣抵代號',
         width: 150,
-        cellClassName: getCellClassName,
+        cellClassName: renderCellClassName,
         editable: true,
     },
     {
-        field: 'otherFee', headerName: '其他金額', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'otherFee', headerName: '其他金額', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
-        field: 'totalAmount', headerName: '總額', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'totalAmount', headerName: '總額', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
-        field: 'totalPayAmount', headerName: '付款總金額', width: 150, cellClassName: getCellClassName, editable: true,
+        field: 'totalPayAmount', headerName: '付款總金額', width: 150, cellClassName: renderCellClassName, editable: true,
     },
     {
         field: 'cellHighlight', hide: true
@@ -169,34 +169,29 @@ function renderEvidenceType() {
         .map(key => {
             const {name} = GW_EVIDENCE_TYPE[key]
             return name
-            // {
-            //     value: key,
-            //     label: name
-            // }
         })
     return result
 }
 
 
-
-function renderDeductionType(param) {
-    const deductionType = param.row.deductionType === undefined || param.row.deductionType === '' ? '1' : param.row.deductionType;
-    return (
-        <FormControl>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                name="deductionType"
-                value={deductionType}
-            >
-                <MenuItem value="1">1 進項可扣抵之進貨及費用</MenuItem>
-                <MenuItem value="2">2 進項可扣抵之固定資產</MenuItem>
-                <MenuItem value="3">3 進項不可扣抵之進貨及費用</MenuItem>
-                <MenuItem value="4">4 進項不可扣抵之固定資產</MenuItem>
-            </Select>
-        </FormControl>
-    );
-}
+// function renderDeductionType(param) {
+//     const deductionType = param.row.deductionType === undefined || param.row.deductionType === '' ? '1' : param.row.deductionType;
+//     return (
+//         <FormControl>
+//             <Select
+//                 labelId="demo-simple-select-label"
+//                 id="demo-simple-select"
+//                 name="deductionType"
+//                 value={deductionType}
+//             >
+//                 <MenuItem value="1">1 進項可扣抵之進貨及費用</MenuItem>
+//                 <MenuItem value="2">2 進項可扣抵之固定資產</MenuItem>
+//                 <MenuItem value="3">3 進項不可扣抵之進貨及費用</MenuItem>
+//                 <MenuItem value="4">4 進項不可扣抵之固定資產</MenuItem>
+//             </Select>
+//         </FormControl>
+//     );
+// }
 
 
 // function renderTaxType(param) {
@@ -228,9 +223,16 @@ function renderSn(cellValues) {
     return <div style={style}>{value}</div>;
 }
 
-function getCellClassName(cellValues) {
+function renderCellClassName(cellValues) {
     if (cellValues.row.cellHighlight && cellValues.row.cellHighlight.indexOf(cellValues.field) > -1) {
-        return 'highlight';
+        return {
+            backgroundColor: '#ff943975',
+            color: '#1a3e72',
+        };
+    }
+    return {
+        backgroundColor: '#ff943975',
+        color: '#1a3e72',
     }
 }
 
