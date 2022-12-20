13df794e197cb81af95a107f985654ad522d5d08
Tony <se11@gateweb.com.tw>
2022-12-20 02:15:11 +0000

    F/package new app

--- src/react/Context/index.js
+++
@@ -1,9 +0,0 @@
-import * as gwActions from '../Actions/gwActions';
-import * as sightTourActions from '../Actions/sightourActions';
-import * as electronActions from '../Actions/electionActions';
-import { AppContextProvider, useAppDispatch, useAppState } from './context';
-import actionTypes from './actionTypes';
-
-export {
-  AppContextProvider, useAppState, useAppDispatch, gwActions, sightTourActions, electronActions, actionTypes,
-};
