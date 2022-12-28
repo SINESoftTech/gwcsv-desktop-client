6f08660fb3a1a39f143a261180f2ac046f7b0b49
Tony <se11@gateweb.com>
2022-12-28 11:49:09 +0800

    fixed url

--- src/react/Actions/axios.js
+++ src/react/Actions/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const GW_ROOT_URL = 'http://34.80.79.216:8080'; // uat //34.80.79.216(dev)
-// const axios = require('axios').default
+const GW_ROOT_URL = 'http://104.155.204.250:8080/';
 const gwAxios = axios.create({
   baseURL: GW_ROOT_URL,
   headers: {'Content-Type': 'application/json'},
